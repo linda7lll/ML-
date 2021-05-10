c70bfee66808a5ffa5f68b41d406b5ea42c2496a
linda7lll <76974134+linda7lll@users.noreply.github.com>
2021-05-10 12:05:39 +0300

    Update metin-ses döüştürme(gTTS).py

--- metin-ses döüştürme(gTTS).py
+++ metin-ses döüştürme(gTTS).py
@@ -9,5 +9,5 @@ Created on Sun May  2 13:39:06 2021
 from gtts import gTTS
 import os #işletim sistemindeki dosyalara erişim için 
 
-gTTS(text = "seni pezevenklerin elinden kurtardım", lang = "tr", slow = False).save("text.mp3")
+gTTS(text = "Veri her yerde.", lang = "tr", slow = False).save("text.mp3")
 os.system("start text.mp3")
